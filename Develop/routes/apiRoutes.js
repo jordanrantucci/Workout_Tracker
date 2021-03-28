46c8ed18564bca169dac23612ef76a9645c5d582
Jordan Rantucci <jordanrantucci@gmail.com>
2021-03-28 15:36:42 -0400

    working on api routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const db = require("../models");
-const router = require("express").Router();
 
-router.get("/api/workouts", (req, res) => {
+module.exports = (app) => {
+app.get("/api/workouts", (req, res) => {
     db.Workout.find({}).then(dbWorkout => {
         dbWorkout.forEach(workout => {
             var total = 0;
@@ -14,5 +14,39 @@ router.get("/api/workouts", (req, res) => {
     }).catch(err => {
         res.json(err)
     })
+})
 
-});
+app.put("/api/workouts:id", ({ body }, res) => {
+    db.Workout.findOneAndUpdate(
+        {_id: body.id},
+        {
+            $inc: { totalDuration: body.duration },
+            $push: { exercises: body }
+        },
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout)
+        }).catch(err => {
+            res.json(err)
+        })
+})
+
+app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+        .then((dbWorkout => {
+            res.json(dbWorkout)
+        }))
+        .catch(err => {
+            res.json(err);
+        })
+    })
+
+app.get("/api/workouts/range", ({ body }, res) => {
+    db.Workout.find({ body }).then(dbWorkout => {
+        res.json(dbWorkout)
+    }).catch(err => {
+        res.json(err)
+    })
+})
+
+}
+
