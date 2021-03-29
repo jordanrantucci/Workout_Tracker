237904e1b094cee77a6589fdf507e54bc7ccad66
Jordan Rantucci <jordanrantucci@gmail.com>
2021-03-28 22:29:43 -0400

    correcting mongoose connect

--- Develop/server.js
+++ Develop/server.js
@@ -15,7 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
 
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
